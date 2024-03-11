43b8e1de4dcfed346bda7aa6652e28c962ca3a61
David Padilla Villén <149684862+Davpad@users.noreply.github.com>
2024-03-11 12:23:03 +0000

    Final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,48 @@
-import React from "react";
+import React,{ useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
+
 //create your first component
 const Home = () => {
+	const [newTask,setNewTask]= useState("")
+	const [tasks,setTasks] = useState([])
+	// console.log(tasks);
+	function addTask(e) {
+		if (e.key === "Enter") {
+			setTasks(tasks.concat(newTask));
+			setNewTask("");
+		}
+	}
+
+	function deleteTask(index){
+		const filteredTasks = tasks.filter((i) => i !== index);
+		setTasks(filteredTasks);
+	}
+
+	const arrTasks = tasks.map((task)=> <li className="justify-content-left">{task}<span className="text-danger fw-bold" onClick={()=>deleteTask(task)}> X</span></li>)
+
+	let number = tasks.length
+
+	let txt = "task"
+	if (number >= 2) {
+		txt = "tasks"
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	<>
+		<div className="container-fluid col-6 bg-white">
+			<h1 className="text-center mt-5">My ToDo List</h1>
+			<ul>
+				<li><input type="text" className="form-control"  onChange={e=>setNewTask(e.target.value)} onKeyDown={addTask} value={newTask} placeholder="What needs to be done?"/></li>
+				{arrTasks}
+				<li className="d-flex"><div className="text-danger fw-bold px-2">{number}</div>{txt} to do.</li>
+			</ul>
+
 		</div>
+	</>
 	);
 };
 
