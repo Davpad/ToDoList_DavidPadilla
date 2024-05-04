5b557a9c1a97cb1625af7741416fcac9eb6d71f1
David Padilla Villén <149684862+Davpad@users.noreply.github.com>
2024-05-04 11:44:39 +0000

    May the fouth

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,6 +10,12 @@ const Home = () => {
 	const [newTask,setNewTask]= useState("")
 	const [tasks,setTasks] = useState([])
 	// console.log(tasks);
+
+	function vadersToDo() {
+		const vadersTasks = ["Find Death Star plans", "Fix the gap in Death Star", "Stormtrooper's shooting lessons", "Eradicate wookies", "Family therapy with Leia and Luke", "Seize Millennium Falcon", "Find Yoda", "Find Obi Wan", "Have Solo arrested for speeding", "Give Lando a bigger brive"];
+		setTasks(vadersTasks);
+	}
+
 	function addTask(e) {
 		if (e.key === "Enter") {
 			setTasks(tasks.concat(newTask));
@@ -34,13 +40,13 @@ const Home = () => {
 	return (
 	<>
 		<div className="container-fluid col-6 bg-white">
-			<h1 className="text-center mt-5">My ToDo List</h1>
+			<h1 className="text-center mt-5">Vader's ToDo List</h1>
 			<ul>
 				<li><input type="text" className="form-control"  onChange={e=>setNewTask(e.target.value)} onKeyDown={addTask} value={newTask} placeholder="What needs to be done?"/></li>
 				{arrTasks}
 				<li className="d-flex"><div className="text-danger fw-bold px-2">{number}</div>{txt} to do.</li>
 			</ul>
-
+			<button type="button" class="btn btn-danger" onClick={vadersToDo}>Load ToDo List</button>
 		</div>
 	</>
 	);
